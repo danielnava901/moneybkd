ba4c083d33ca2bcaefafcd95818173187f99a7f5
Daniel Nava Viveros <daniel.navaviveros@nutanix.com>
2025-12-04 13:19:54 -0600

    vercel json3

--- api/index.go
+++ api/index.go
@@ -1,11 +1,14 @@
 package api
 
 import (
+	"log"
 	"moneybkd/server"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("INIT HANDLER...............")
 	var echo = server.New()
 	echo.ServeHTTP(w, r)
+	log.Println("END INIT HANDLER...............")
 }
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"log"
 	"moneybkd/config"
 	"moneybkd/controllers"
 	"moneybkd/repository"
@@ -13,22 +14,27 @@ import (
 )
 
 func New() *echo.Echo {
+	log.Println("INIT NEW SERVER..............1.")
 	config.ConnectSupabase()
 	countryRepo := repository.NewCountryRepository(config.Supabase)
 	countryHistory := repository.NewHistoryRepository(config.Supabase)
 	apiKey := os.Getenv("EXCHANGE_API_KEY")
-
+	log.Println("INIT NEW SERVER..............2.")
 	svc := service.NewCurrencyService(countryRepo, countryHistory, apiKey)
 	ctrl := controllers.NewCurrencyController(svc)
 
+	log.Println("INIT NEW SERVER..............3.")
 	e := echo.New()
 	e.Use(middleware.CORS())
 
+	log.Println("INIT NEW SERVER..............4.")
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{
 			"data": "Data Ok",
 		})
 	})
+
+	log.Println("INIT NEW SERVER..............5.")
 	e.GET("/currency/:code", ctrl.GetCurrency)
 	e.GET("/currency/:code/history", ctrl.GetHistory)
 	e.GET("/countries", ctrl.GetCountries)
