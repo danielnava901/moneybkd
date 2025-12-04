4d08b786f5357ad3842fdebb6f1781295ba90a16
Daniel Nava Viveros <daniel.navaviveros@nutanix.com>
2025-12-04 13:51:45 -0600

    lgos

--- api/index.go
+++ api/index.go
@@ -7,7 +7,14 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("INIT HANDLER...............")
+	log.Println("INIT HANDLER HOST...............")
+	log.Println(r.Host)
+	log.Println("METHOD...............")
+	log.Println(r.Method)
+	log.Println("URL...............")
+	log.Println(r.URL)
+	log.Println("URL PATH...............")
+	log.Println(r.URL.Path)
 	var echoServer = server.New()
 	echoServer.ServeHTTP(w, r)
 	log.Println("END INIT HANDLER...............")
